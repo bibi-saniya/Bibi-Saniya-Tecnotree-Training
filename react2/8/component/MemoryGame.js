8b3d4d4076fa3b6792a78b802344ceee6bc82431
Bibi Saniya <77954441+bibi-saniya@users.noreply.github.com>
2023-04-05 13:14:10 +0530

    Delete react2 directory

--- react2/8/component/MemoryGame.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import MemoryGame from './component/MemoryGame';
-
-const App = () => {
-  const cards = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-  return <MemoryGame cards={cards} />;
-};
-
-export default App;
